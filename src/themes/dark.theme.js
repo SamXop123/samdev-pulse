8b6bd093a7ec4ba402346603add7fb3299ee6bb5
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-01-27 19:48:47 +0530

    Create dark.theme.js

---
+++ src/themes/dark.theme.js
@@ -0,0 +1,14 @@
+const darkTheme = {
+  name: 'dark',
+  colors: {
+    background: '#0d1117',
+    cardBackground: '#161b22',
+    border: '#30363d',
+    primaryText: '#e6edf3',
+    secondaryText: '#8b949e',
+    accent: '#58a6ff',
+  },
+};
+
+export default darkTheme;
+
